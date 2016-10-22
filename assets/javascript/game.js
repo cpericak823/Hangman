d2ea41b099435973cace551aad5ac816d6c9e3ea
Cassie <cassie.pericak@northwestern.edu>
2016-10-22 09:48:53 -0500

    updated letters guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -46,79 +46,26 @@ document.onkeyup = function(event){
 	lettersGuessed.push(userGuess);
 	document.getElementById("userGuess").innerHTML = 'Letters Guessed: ' + lettersGuessed
 
-
 //add the letters clicked to the dom
 	if (computerWord.indexOf(userGuess) > -1);{
 		computerWord = chosenMovie.replace(/_/g,"[a-z]");
 		allowedGuesses--;
 	}
+	lettersGuesses();
 }
 // Updates the "hangmanWordArea" div to contain the current word "blanked out"
 // (using "_" characters) except for the letters they have gotten correct
-function refreshMatchedLetters() {
-  // Create a new emtpy string
-  // For each letter in the currentWord (sounds like a for loop)
-  // If they have guessed the letter, then concatenate that letter on the
-  // new empty string you've created, otherwise concatenate "_"
-  // Update the inner html of the hangmanWordArea to equal this new string
+function lettersGuesses() { 
+	console.log ("we made it")
+	var computerWordElement = document.getElementById("computerWord");
+	for (i = 0; i < chosenMovie.length; i++) {
+		console.log("letters" + lettersGuessed[i]);
+		console.log ("words" + chosenMovie[i]);
+		if (lettersGuessed[i] === chosenMovie[i]) {
+			computerWord[i] = lettersGuessed[i];
+		}
+	}
 }
 
-// By default, in the html (when the page loads) this div will initially
-// contain a "welcome" message: "Welcome to Hangman, press any key to play!"
-// This will be overwritten by this method once they start playing their first
-// game
 
-// Checks to see if user has won, is still guessing, or has lost and displays
-// The proper message in the message div accrodingly based on one of those
-// 3 outcomes.
-function refreshWinOrLossed() {
-  // Check to see if user has won by calling hasUserWon()
-  // If hasUserWon is true
-      // update messageDiv innerHTML to show a "You won!" message
-  // Else if there are guesses left
-      // set the innerHTML of the message div to an empty string
-      // (game is still in progress, no message to show)
-  // Else
-      // They have run out of guesses and lost, display a "You Lost" message
-}
-
-// Returns true if the hangmanWordAreaDiv's inner html === the currentWord
-// Increments wins if they've won
-// Sets guessesRemaining to zero, so next keypress starts a new game
-// Else returns false
-function hasUserWon() {
-
-}
-
-// Takes 1 argument --> letterGuessed
-// Checks to see if letter exists in currentWord
-// Properly adds the guess to guessedLetters & matchedLetters
-// Increments totalGuesses
-// Decrements guessesLeft
-function matchGuess(letterGuessed) {
-
-}
-
-// Takes 1 argument --> the letter guessed
-// Returns true if the letterGuessed is in the array of validLetters
-function isAValidLetter(letterGuessed) {
-
-}
-
-// Takes 1 argument -> the letter guessed
-// Checks to see if the letter guessed is in the array of guessed letters
-// Alerts ("you've already guessed this letter") if they have already
-// Returns true if they have, returns false if they have not
-function hasGuessedLetter(letterGuessed) {
-
-}
-
-  // If guessesLeft is 0
-      // call resetGame()
-
-  // Else
-      // Parse out the letterGuessed from the event
-      // If it is a validLetter && the user has not guessed the letter yet
-          // call matchGuess() to match the guess with the word
-
-  // call refreshViewGameStats() on each keyup
+	
